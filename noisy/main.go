2a13881c3bae3ad77a8540d5cca62b9fe46352b4
Ian Howell <ian.howell0@gmail.com>
2024-10-10 12:47:58 +0000

    Make the log demo more realistic

--- noisy/main.go
+++ noisy/main.go
@@ -1,37 +1,71 @@
 package main
 
 import (
-	"errors"
 	"log"
 	"os"
+
+	"github.com/pkg/errors"
 )
+
+// NOTE(ianhowell): global state for DEMO PURPOSES ONLY.
+var numAttempts = 5
+
+type Cluster struct {
+	Node *Node // we only have a single node to keep the example simple
+}
+type Node struct {
+	Volume *Volume
+}
+type Volume struct{}
 
 func main() {
 	log.SetOutput(os.Stdout)
-
-	a, b := 10, 0
-	// Simulate 15 failures
-	for i := 0; i < 15; i++ {
-		_, err := divide(a, b)
-		if err != nil {
-			log.Printf("could not divide %d by %d: %v", a, b, err)
+	done := false
+	for !done {
+		done = true
+		if err := Reconcile(&Cluster{Node: &Node{}}); err != nil {
+			log.Printf("Failed to reconcile cluster: %v", err)
+			done = false
 		}
 	}
-
-	// Follow it up with a single success
-	b = 2
-	result, err := divide(a, b)
-	if err != nil {
-		log.Printf("could not divide %d by %d: %v", a, b, err)
-	}
-	log.Printf("result: %d\n", result)
 }
 
-func divide(a, b int) (int, error) {
-	log.Println("dividing")
-	if b == 0 {
-		return 0, errors.New("divide by zero")
+func Reconcile(c *Cluster) error {
+	log.Println("Reconciling cluster")
+	if err := UpdateNode(c.Node); err != nil {
+		return errors.Wrap(err, "failed to update node")
 	}
-	log.Println("divided")
-	return a / b, nil
+	log.Println("Reconciled cluster")
+	return nil
+}
+
+func UpdateNode(n *Node) error {
+	log.Println("Updating node")
+	if err := AttachVolume(n); err != nil {
+		return errors.Wrap(err, "failed to attach volume")
+	}
+	log.Println("Updated node")
+	return nil
+}
+
+func AttachVolume(n *Node) error {
+	log.Println("Attaching volume")
+	v, err := CreateVolume()
+	if err != nil {
+		return errors.Wrap(err, "failed to create volume")
+	}
+	n.Volume = v
+	log.Println("Attaching volume")
+	return nil
+}
+
+func CreateVolume() (*Volume, error) {
+	log.Println("Creating volume")
+	if numAttempts > 0 {
+		numAttempts--
+		// simulate an error
+		return nil, errors.Errorf("something terrible has happened 😮")
+	}
+	log.Println("Created volume")
+	return &Volume{}, nil
 }
--- quiet/main.go
+++ quiet/main.go
@@ -1,36 +1,67 @@
 package main
 
 import (
-	"errors"
 	"log"
 	"os"
+
+	"github.com/pkg/errors"
 )
+
+// NOTE(ianhowell): global state for DEMO PURPOSES ONLY.
+var numAttempts = 5
+
+type Cluster struct {
+	Node *Node // we only have a single node to keep the example simple
+}
+type Node struct {
+	Volume *Volume
+}
+type Volume struct{}
 
 func main() {
 	log.SetOutput(os.Stdout)
-
-	a, b := 10, 0
-	// Simulate 15 failures
-	for i := 0; i < 15; i++ {
-		_, err := divide(a, b)
-		if err != nil {
-			log.Printf("could not divide %d by %d: %v", a, b, err)
+	done := false
+	for !done {
+		done = true
+		if err := Reconcile(&Cluster{Node: &Node{}}); err != nil {
+			log.Printf("Failed to reconcile cluster: %v", err)
+			done = false
 		}
 	}
-
-	b = 2
-	result, err := divide(a, b)
-	if err != nil {
-		log.Printf("could not divide %d by %d: %v", a, b, err)
-	}
-	// Follow it up with a single success
-	log.Printf("result: %d\n", result)
 }
 
-func divide(a, b int) (int, error) {
-	if b == 0 {
-		return 0, errors.New("divide by zero")
+func Reconcile(c *Cluster) error {
+	if err := UpdateNode(c.Node); err != nil {
+		return errors.Wrap(err, "failed to update node")
 	}
-	log.Println("divided")
-	return a / b, nil
+	log.Println("Reconciled cluster")
+	return nil
+}
+
+func UpdateNode(n *Node) error {
+	if err := AttachVolume(n); err != nil {
+		return errors.Wrap(err, "failed to attach volume")
+	}
+	log.Println("Updated node")
+	return nil
+}
+
+func AttachVolume(n *Node) error {
+	v, err := CreateVolume()
+	if err != nil {
+		return errors.Wrap(err, "failed to create volume")
+	}
+	n.Volume = v
+	log.Println("Attaching volume")
+	return nil
+}
+
+func CreateVolume() (*Volume, error) {
+	if numAttempts > 0 {
+		numAttempts--
+		// simulate an error
+		return nil, errors.Errorf("something terrible has happened 😮")
+	}
+	log.Println("Created volume")
+	return &Volume{}, nil
 }
